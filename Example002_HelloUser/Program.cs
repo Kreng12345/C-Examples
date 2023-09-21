0fb1f843d9e9ece0cb38caf107c4e353c08002c6
73dave <73dave@mail.ru>
2023-09-21 23:25:24 +0300

    add Example002 Hello User project

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите ваше имя ");// writeln выведет одну строку с текстом
+string username = Console.ReadLine();// во второй строке ввод имени
+Console.Write("Привет, ");
+Console.Write(username);// write выведет приветствие и имя в одну строку
