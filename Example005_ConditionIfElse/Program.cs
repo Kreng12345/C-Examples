5e2b928a48ff5f917914e87b46c680020b656672
73dave <73dave@mail.ru>
2023-09-22 01:20:07 +0300

    the name is now case-insensitive

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter the user name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha") // теперь условие не чуствительно к регистру (m M)
+{
+    Console.WriteLine("Wow, it's Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
